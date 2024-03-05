fe045788414f49acb780c7ae4918e11f045eb3c8
Ubuntu <sumair.aslam@codingcops.com>
2024-03-05 12:26:23 +0500

    Checks and calculations updated for profit calculations

---
+++ src/jsx/components/Dashboard/Home.js
@@ -0,0 +1,143 @@
+import React, { useContext, useEffect, useState } from "react";
+import * as web3 from '@solana/web3.js'; // Ensure web3 is imported
+import { Link } from "react-router-dom";
+import "swiper/css";
+import { ThemeContext } from "../../../context/ThemeContext";
+import { useTranslation } from "react-i18next";
+import coin from "./../../../images/coin.png";
+import Loader from "../Loader/Loader";
+import SwiperSlider from "../Swipers/SwiperSlider";
+import axios from "axios";
+import jwt from 'jsonwebtoken';
+import axiosInstance from "../../../services/AxiosInstance";
+
+
+const Home = () => {
+  const [bxgavailable, setbxgavailable] = useState(null);
+  const [bxgstacked, setbxgstacked] = useState(null);
+  const [totalEarning, settotalEarning] = useState(null);
+  const [referralBonus, setreferralBonus] = useState(null);
+  const [stakingreferralBonus, setStakingReferralBonus] = useState(null);
+  const [rewardBonus, setRewardBonus] = useState(null);
+  const [loader, setLoader] = useState(true); // Initially true to show loader while data is fetched
+  const [todayProfit, setTodayProfit] = useState(0);
+  const [weeklyProfit, setWeeklyProfit] = useState(0);
+  const [monthlyProfit, setMonthlyProfit] = useState(0);
+  const [solPrice,setSolPrice] = useState()
+  const { changeBackground } = useContext(ThemeContext);
+  
+  useEffect(() => {
+    changeBackground({ value: "dark", label: "Dark" });
+
+    const fetchWalletDetails = async () => {
+      try {
+        const response = await window.solana.connect(); // Connect to Solana wallet
+        const address = response.publicKey.toString();
+        console.log("Wallet Address:", address);
+
+        const connection = new web3.Connection(web3.clusterApiUrl('devnet'));
+        console.log("connection", connection);
+
+        const balanceInLamports = await connection.getBalance(response.publicKey);
+        const balance = balanceInLamports / web3.LAMPORTS_PER_SOL;
+        console.log("Balance in SOL:", balance);
+
+        // Update state with fetched balance
+        setbxgavailable(balance);
+            const getuserData = await axios.get(`http://localhost:4000/api/bxg/${address}`)
+            const currentDate = new Date();
+            const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+            const formattedDate = currentDate.toLocaleDateString('en-GB', options);
+            
+           const {day1,day7}  = getuserData.data
+
+           if (day1 === null && day7 === null) {
+            const updateBalance = await axios.put(`http://localhost:4000/api/bxg/${getuserData.data.id}`, { day1: `${formattedDate}:${balance}` })
+            console.log("updateBalance: ",updateBalance);
+          } else {
+           let apiDate =  day1.split(":")[0] 
+          if(apiDate == formattedDate ){
+            const updateBalance = await axios.put(`http://localhost:4000/api/bxg/${getuserData.data.id}`, { day1: `${formattedDate}:${balance}` })
+            console.log(updateBalance.data);
+          } else{
+
+            const updateBalance = await axios.put(`http://localhost:4000/api/bxg/${getuserData.data.id}`, { day7: `${formattedDate}:${balance}` })
+            console.log(updateBalance.data);
+
+          }
+            
+          }
+            let today = balance;//parseFloat(getuserData.data.day1.split(':')[1])
+            console.log("USer data: ", getuserData)
+            console.log("TOday: ",today )
+            console.log("Today's profit: ",today - (getuserData.data.bxg - 0.1));
+            let profit = today - (getuserData.data.bxg - 0.1);
+            if((profit + getuserData.data.bxg) > (getuserData.data.bxg - 0.1)){
+              setTodayProfit(profit)
+            }
+            else{
+              setTodayProfit(0);
+            }
+            
+            if(getuserData.data.day7 == null){
+              setWeeklyProfit(0)
+            }else{
+              let weekly = parseFloat(getuserData.data.day7.split(':')[1])
+              setWeeklyProfit(weekly - (getuserData.data.bxg - 0.1)); 
+            }
+            console.log("getuserData.data.day1: ", getuserData.data.day1.split(":")[0])
+            console.log("formattedDate.slice(3,5): ", formattedDate.slice(3,5))
+
+            if(getuserData.data.day1.split(":")[0].slice(3, 5) != formattedDate.slice(3,5)){
+              setWeeklyProfit(0)
+            }else{
+              let monthly = parseFloat(getuserData.data.day7.split(':')[1])
+              setMonthlyProfit(monthly - (getuserData.data.bxg - 0.1)); 
+            }
+
+            const usdPrice = await axios.get(
+              'https://api.coincap.io/v2/assets/solana'
+            );
+             console.log(usdPrice.data.data.priceUsd);
+              setSolPrice(usdPrice.data.data.priceUsd)
+      } catch (error) {
+        console.error('Error fetching wallet details:', error);
+    
+      } finally {
+        setLoader(false);
+      }
+    };
+
+
+    const interval = setInterval(fetchWalletDetails, 7000); // Run fetchWalletDetails every 5 seconds
+
+    // Cleanup function to clear the interval when component unmounts
+    return () => clearInterval(interval);
+
+  }, []);
+
+  return (
+    <>
+      {loader ? (
+        <Loader />
+      ) : (
+        <div className="row">
+          <div className="col-xl-12">
+            <SwiperSlider
+              bxgavailable={bxgavailable}
+              bxgstacked={bxgstacked}
+              totalEarning={totalEarning}
+              todayProfit = {todayProfit}
+              weeklyProfit={weeklyProfit}
+              monthlyProfit={monthlyProfit}
+              solPrice={solPrice}
+              // Pass other props as needed
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Home;
---
+++ src/jsx/components/Swipers/SwiperSlider.js
@@ -0,0 +1,148 @@
+import { useTranslation } from "react-i18next";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperSlideCard from "./SwiperSlideCard";
+import { useEffect } from "react";
+import axios from "axios";
+
+const SwiperSlider = ({
+  bxgavailable,
+  bxgstacked,
+  referralBonus,
+  stakingreferralBonus,
+  totalEarning,
+  RewardMEMES,
+  rewardBonus,
+  todayEarning,
+  todayProfit,
+  weeklyProfit,
+  monthlyProfit,
+  solPrice
+}) => {
+
+  
+ 
+
+  return (
+    <div className="col-xl-12">
+      <Swiper
+        className="mySwiper"
+        speed={1500}
+        slidesPerView={3}
+        spaceBetween={20}
+        loop={false}
+        //autoplay= {{
+        //delay: 1200,
+        //}}
+        //modules={[ Autoplay ]}
+        breakpoints={{
+          300: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          416: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1200: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+
+          1500: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+          1788: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+          },
+        }}
+      >
+        <SwiperSlide>
+          <SwiperSlideCard
+            amount={bxgavailable}
+            translateKey="dashboard_card_1"
+            currency="SOL"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SwiperSlideCard
+            amount={ todayProfit}
+            translateKey="dashboard_card_3"
+            currency=" SOL"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SwiperSlideCard
+            amount={weeklyProfit}
+            translateKey="dashboard_card_4"
+            currency=" SOL"
+          />
+        </SwiperSlide>
+   
+      </Swiper>
+      <Swiper
+        className="mySwiper"
+        speed={1500}
+        slidesPerView={3}
+        spaceBetween={20}
+        loop={false}
+        //autoplay= {{
+        //delay: 1200,
+        //}}
+        //modules={[ Autoplay ]}
+        breakpoints={{
+          300: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          416: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1200: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+
+          1500: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+          1788: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+          },
+        }}
+      >
+        <SwiperSlide>
+          <SwiperSlideCard
+            amount={monthlyProfit}
+            translateKey="dashboard_card_5"
+            currency="SOL"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SwiperSlideCard
+            amount={bxgavailable*solPrice}
+            translateKey="Total Holdings in USD ($)"
+            currency="$ USD"
+          />
+        </SwiperSlide>
+       
+   
+      </Swiper>
+   
+    </div>
+  );
+};
+
+export default SwiperSlider;
