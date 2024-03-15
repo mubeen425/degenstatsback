cb432bf0132c9c59c25cd5c8f924f504af154fd3
Ubuntu <sumair.aslam@codingcops.com>
2024-03-16 01:29:18 +0500

    consol log

--- utils/connection.js
+++ utils/connection.js
@@ -1,6 +1,8 @@
 const config = require("config");
 const { Sequelize } = require("sequelize");
 const { DATABASE, USERNAME, PASSWORD, HOST, PORT } = config.get("DBCONFIG");
+console.log("Databse: ", DATABASE)
+console.log("Host: ", HOST)
 
 module.exports = new Sequelize(DATABASE, USERNAME, PASSWORD, {
   host: HOST,
